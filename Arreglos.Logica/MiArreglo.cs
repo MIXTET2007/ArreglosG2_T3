c540c389b91e405936a48f6700e744b88dc004c1
MIXTET2007 <25030271@itesa.edu.mx>
2026-09-23 15:01:03 -0600

    version_03: Metodo ordenar ascendente y descendente

--- Arreglos.Logica/MiArreglo.cs
+++ Arreglos.Logica/MiArreglo.cs
@@ -1,10 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Arreglos.Logica
 {
-    internal class MiArreglo
+    public class MiArreglo
     {
+        //Atributos o campos
+        private int _tope;
+        private int[] _arreglo;
+
+
+        //constructor
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[N];
+            _tope = 0;
+
+        }
+
+        //Propiedades
+        public int N { get; }
+
+        public bool EstaVacio => _tope == 0;
+        public bool EstaLleno => _tope == N;
+
+        //Metodos 
+        //Metodos LLenar con parametros
+        public void Llenar(int minimo, int maximo)
+        {
+            Random oRandom = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = oRandom.Next(minimo, maximo);
+            }
+            _tope = N;
+        }
+        //Metodo Ordenar
+        public void Ordenar()
+        {
+            Ordenar(true);
+        }
+
+        //Metodo ordenar
+        public void Ordenar(bool ascendente)
+        {
+            for(int i = 0; i < _tope; i++)
+            {
+                for(int j = 0; j < _tope; j++)
+                {
+                    if (ascendente)
+                    {
+                        if (_arreglo[i] > _arreglo[j])
+                        {
+                            Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        }
+                    }
+                    else
+                    {
+                        if (_arreglo[i] < _arreglo[j])
+                        {
+                            Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Metodo cambiar
+        public void Cambiar(ref int a, ref int b)
+        {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                return "El arreglo esta vacio";
+            }
+            string salida = string.Empty;
+
+            int contador = 0;
+
+            for (int i = 0; i < _tope; i++)
+            {
+                salida += $"{_arreglo[i]}\t";
+                contador++;
+
+                if (contador > 9)
+                {
+                    salida += "\n";
+                }
+            }
+            return salida;
+        }
+        
     }
 }
--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,8 +1,26 @@
-﻿internal class Program
+﻿using Arreglos.Logica;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nArreglos");
+             
+        MiArreglo oMiArreglo = new MiArreglo(10);
+
+        oMiArreglo.Llenar(5, 20);
+        Console.WriteLine("\nArreglo Desordenado");
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine("\nArreglo ordenado descendente");
+        oMiArreglo.Ordenar();
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine("\nArreglo ordenado ascendente");
+        oMiArreglo.Ordenar(false);
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine(oMiArreglo.ToString);
         Console.ReadKey();
     }
 }
